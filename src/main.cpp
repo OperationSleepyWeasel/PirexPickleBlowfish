23701b2358ede229a40f2d45a4c77e190e01be3d
owca <artus.sawicki@gmail.com>
2013-12-16 21:56:30 +0100

    trivial logging added

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,20 @@
 #include "logic/Dollar.hpp"
 //#include "gui/BoardSample.hpp"
 #include "gui/MainWindowSample.hpp"
+#include "Logger.hpp"
+#include <boost/log/trivial.hpp>
 
 int main(int argc, char* argv[])
 {
+  initLogging();
+
+  BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
+  BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
+  BOOST_LOG_TRIVIAL(info) << "An informational severity message";
+  BOOST_LOG_TRIVIAL(warning) << "A warning severity message";
+  BOOST_LOG_TRIVIAL(error) << "An error severity message";
+  BOOST_LOG_TRIVIAL(fatal) << "A fatal severity message";
+
   QApplication app(argc, argv);
   //BoardSample boardSample;
   //boardSample.show();
